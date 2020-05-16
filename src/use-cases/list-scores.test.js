a664b3f5a181c0d72d443aeecbf876e2eee63994
kwdowik <wdowikkacper@gmail.com>
2020-05-16 19:20:37 +0200

    Fix list-scores tests

--- src/use-cases/list-scores.test.js
+++ src/use-cases/list-scores.test.js
@@ -1,22 +1,26 @@
 import makeListScores from './list-scores'
 import makeScoresDb from '../data-access/scores-db'
 import makeFakeScore from '../../__test__/fixtures/score'
-import makeDb from '../../__test__/fixtures/db'
+import makeDb, { clearDb } from '../../__test__/fixtures/db'
 
-describe.skip('list scores', () => {
+describe('list scores', () => {
   let scoresDb
-  beforeAll(() => {
+  beforeAll(async () => {
+    await makeDb()
+    await clearDb()
     scoresDb = makeScoresDb({ makeDb })
   })
 
   it('find all scores from the database', async () => {
     const newScore1 = makeFakeScore();
     const newScore2 = makeFakeScore();
-    scoresDb.insert(newScore1);
-    scoresDb.insert(newScore2);
+    await scoresDb.insert(newScore1);
+    await scoresDb.insert(newScore2);
     const listScores = makeListScores({
       scoresDb: scoresDb,
     })
-    expect(await listScores()).toMatchObject([newScore1, newScore2])
+    const scores = await listScores();
+    expect(scores.length).toBe(2)
+    expect(scores).toEqual([newScore1, newScore2])
   })
 })
